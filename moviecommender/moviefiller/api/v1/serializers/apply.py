d2f382f9c10b30df59a68744e855f0e73240bd0a
Ajay Pokharel <ajaypokharel8@gmail.com>
2021-06-09 23:13:56 +0545

    [add] overrides get_extra_kwargs and create method in the serializer

--- moviecommender/moviefiller/api/v1/serializers/apply.py
+++ moviecommender/moviefiller/api/v1/serializers/apply.py
@@ -44,10 +44,31 @@ class MovieFillerSerializer(DynamicFieldsModelSerializer):
 
 class MovieFillerApplySerializer(DynamicFieldsModelSerializer):
     applicant = UserRegisterationSerializer(fields=['email', 'first_name', 'last_name', 'display_name', 'birthdate',
-                                                    'current_address', 'gender', 'profile_picture', 'bio', 'fav_genre'])
+                                                    'current_address', 'gender', 'profile_picture', 'bio', 'fav_genre'],
+                                            read_only=True)
 
     class Meta:
         model = MovieFillerApply
         fields = ['uuid', 'applicant', 'reason', 'bio', 'status']
         read_only_fields = ['uuid', 'applicant', 'status']
 
+    def get_extra_kwargs(self):
+        extra_kwargs = super().get_extra_kwargs()
+        view = self.context.get('view')
+        if view and view.action in ['partial_update', 'update']:
+            extra_kwargs['status'] = {
+                'write_only': True
+            }
+        return extra_kwargs
+
+
+    def create(self, validated_data):
+        validated_data['applicant'] = self.request.user
+        instance = super().create(validated_data)
+        return instance
+
+    def update(self, instance, validated_data):
+        status = validated_data.get('status')
+        if status == 'ACCEPTED':
+            MovieFiller.objects.create(filler=self.request.user)
+        return instance
