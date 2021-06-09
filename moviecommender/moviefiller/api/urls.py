38ecb2ef830d8e100edb2b9acce08ec6094fbd82
Ajay Pokharel <ajaypokharel8@gmail.com>
2021-06-09 23:16:26 +0545

    [add] registers apply viewsets in the urls

--- moviecommender/moviefiller/api/urls.py
+++ moviecommender/moviefiller/api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
+from .v1.views.apply import MovieFillerApplyViewSet
 from .v1.views.movie_filler import MovieFillerViewSet
 
 r = DefaultRouter()
 
 r.register('movie-filler', MovieFillerViewSet, basename='movie-filler')
+r.register('apply', MovieFillerApplyViewSet, basename='apply')
 
 urlpatterns = r.urls
